a436a4f9e19ce847a130c3997ccff66786a61830
LeStitcheurFou <mcfly@mywebsoluce.fr>
2022-02-22 17:16:30 +0100

    Quizz + page perso v2

---
+++ script.bak/alex.js
@@ -0,0 +1,20 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "flex";
+  modal.style.top = "0";
+  modal.style.opacity = "1";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
